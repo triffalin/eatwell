dd3d51fa9a64a0ac0d4ba83454b9e885840f6318
Alin Triff <triffalin@gmail.com>
2023-09-23 16:19:59 +0300

    showed a 404 page for not found cases

--- app.js
+++ app.js
@@ -39,6 +39,8 @@ app.get('/restaurants/:id', function (req, res) {
 			return res.render('restaurant-detail', { restaurant: restaurant });
 		}
 	}
+
+	res.render('404');
 });
 
 app.get('/recommend', function (req, res) {
