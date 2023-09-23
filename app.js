5cae5b11cbd9493db1d34b34b2cbb605499c6a2b
Alin Triff <triffalin@gmail.com>
2023-09-23 16:10:49 +0300

    loaded and displayed detail data

--- app.js
+++ app.js
@@ -29,7 +29,16 @@ app.get('/restaurants', function (req, res) {
 
 app.get('/restaurants/:id', function (req, res) {
 	const restaurandID = req.params.id;
-	res.render('restaurant-detail', { rid: restaurandID });
+	const filePath = path.join(__dirname, 'data', 'restaurants.json');
+
+	const fileData = fs.readFileSync(filePath);
+	const storedRestaurants = JSON.parse(fileData);
+
+	for (const restaurant of storedRestaurants) {
+		if (restaurant.id === restaurandID) {
+			return res.render('restaurant-detail', { restaurant: restaurant });
+		}
+	}
 });
 
 app.get('/recommend', function (req, res) {
